8c1fbb155a0f4e05863465901845dc5f81f27a20
Kashish  Gupta <gptkashish973@gmail.com>
2026-09-16 14:39:28 +0000

    Variables and 02_dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "hitesg@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+/*
+var is not prefered bcz of issue in block scope and functional scope
+*/
+
+//accountId = 2 // not allowed
+accountEmail = "hitesg@gmail.com"
+accountPassword = "2893e636"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+console.log(accountEmail)
+console.table([accountPassword, accountCity, accountState]);
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat code as newer js version
+
+// alert(3+3) // we are using nodejs, notb browser
+console.log(3+3); console.log("Hitesh") // code readability
+
+let name = " Hitesg"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+// string
+//boolean
+// null => standalone value 
+// undefined => value not assigned
+// symbol=> uniquueness define
+
+// object
+
+console.log(typeof "Hites")
+console.log(typeof null)
+console.log(typeof undefined)
